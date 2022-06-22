c94b8f066d9daec4e1f5d0f72f8b302dee6f910e
Andy Snell <andy@wickedbyte.com>
2022-06-22 15:23:50 -0500

    Add Basic Up Command

--- app/Http/Controllers/MoveController.php
+++ app/Http/Controllers/MoveController.php
@@ -9,6 +9,6 @@ class MoveController extends Controller
 {
     public function __invoke(Request $request): JsonResponse
     {
-        return new JsonResponse(['controller' => 'move']);
+        return new JsonResponse(['move' => 'up']);
     }
 }
