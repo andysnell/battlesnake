f47cf91bb7704b4fe425a44d382315e6f736601f
Andy Snell <andy@wickedbyte.com>
2022-06-22 15:43:59 -0500

    log_payload

--- app/Http/Controllers/MoveController.php
+++ app/Http/Controllers/MoveController.php
@@ -4,12 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Psr\Log\LoggerInterface;
 
 class MoveController extends Controller
 {
+    private LoggerInterface $logger;
+
+    public function __construct(LoggerInterface $logger)
+    {
+
+        $this->logger = $logger;
+    }
+
     public function __invoke(Request $request): JsonResponse
     {
-        logger()->debug('payload', [
+        $this->logger->info('payload', [
             'request' => $request->toArray()
         ]);
 
